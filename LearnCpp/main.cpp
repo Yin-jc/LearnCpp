de95d3248b765921fa808fb549d1647b67c6c2e5
yinjiacheng <yinjiacheng@yidian-inc.com>
2021-03-21 00:22:48 +0800

    Initial Commit

---
+++ LearnCpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LearnCpp
+//
+//  Created by admin on 2021/3/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

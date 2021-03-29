2ba7658fbcf694b964edfec31c697a350bd39170
yinjiacheng <yinjiacheng@yidian-inc.com>
2021-03-29 18:50:33 +0800

    Learn cin cout

--- LearnCpp/main.cpp
+++ LearnCpp/main.cpp
@@ -5,10 +5,31 @@
 //  Created by admin on 2021/3/21.
 //
 
-#include <iostream>
+#include <iostream> // a preprocess directive
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    using namespace std;
+//    cout << "Come up and C++ me some time.";
+//    cout << endl; // start a new line
+//    cout << "You won't regret it!" << endl;
+    
+    int carrots;
+    cout << "How many carrots do you have?" << endl;
+    cin >> carrots;
+    cout << "Here are you more. ";
+    carrots = carrots + 2;
+    cout << "Now you have "
+         << carrots
+         << " carrots."
+         << endl;
     return 0;
 }
+
+//original C style
+//default return int
+//main() {
+//    return 0;
+//}
+
+
